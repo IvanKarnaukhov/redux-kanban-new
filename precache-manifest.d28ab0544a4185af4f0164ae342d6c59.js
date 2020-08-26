e79294782637e6edb20f3b3432a450db056d5d60
Ivan K <69258747+IvanKarnaukhov@users.noreply.github.com>
2020-08-25 23:30:34 -0700

    Updates

---
+++ precache-manifest.d28ab0544a4185af4f0164ae342d6c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9941b825f2c29ebaf71a379e9c6271f1",
+    "url": "/redux-kanban-new/index.html"
+  },
+  {
+    "revision": "cf92d68a525c5fccbf85",
+    "url": "/redux-kanban-new/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a2892d332533ea963b36",
+    "url": "/redux-kanban-new/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf92d68a525c5fccbf85",
+    "url": "/redux-kanban-new/static/js/2.30ebbb9d.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/redux-kanban-new/static/js/2.30ebbb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2892d332533ea963b36",
+    "url": "/redux-kanban-new/static/js/main.ea7042c0.chunk.js"
+  },
+  {
+    "revision": "77a8201d7eb6bf3a2787",
+    "url": "/redux-kanban-new/static/js/runtime-main.5a968ec2.js"
+  }
+]);
